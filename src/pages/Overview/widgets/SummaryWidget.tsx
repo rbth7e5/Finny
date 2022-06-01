f40466b178218d53b2695c6bc98d4eb469b3b10a
Chen Caijie <caijie96@gmail.com>
2022-06-02 01:53:19 +0800

    update summary widget

--- src/pages/Overview/widgets/SummaryWidget.tsx
+++ src/pages/Overview/widgets/SummaryWidget.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import WidgetWrapper from './WidgetWrapper';
-import { Text } from 'react-native-paper';
+import { Headline, Text } from 'react-native-paper';
 
 const SummaryWidget = () => {
   return (
     <WidgetWrapper>
       <Text>You spent...</Text>
-      <Text>$100 this week</Text>
-      <Text>$900 this month</Text>
+      <Headline>$100</Headline>
+      <Text>this week</Text>
     </WidgetWrapper>
   );
 };
